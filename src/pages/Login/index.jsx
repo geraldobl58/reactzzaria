aec9ebd3ca4ccc096d297295bc040aff8382015c
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-28 14:08:31 -0300

    Grid on the login screen

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 
 import { ReactComponent as Logo } from '../../images/logo-react-zzaria.svg'
 
 const Login = () => (
   <React.Fragment>
-    <Logo />
-    <Button>Entrar com GitHub</Button>
+    <Grid container>
+      <Grid item>
+        <Logo />
+      </Grid>
+      <Grid item>
+        <Button>Entrar com GitHub</Button>
+      </Grid>
+    </Grid>
   </React.Fragment>
 )
 
