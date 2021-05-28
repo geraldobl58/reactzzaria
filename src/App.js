66112c5d3a7ea9a75b1b1297b0671e9b33929355
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-28 12:20:55 -0300

    Creating first routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import MainPage from 'pages/Main';
+import Login from 'pages/Login';
+
 const App = () => (
-  <h1>React</h1>
+  <BrowserRouter>
+    <Switch>
+      <Route path='/login' component={Login} />
+      <Route component={MainPage} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App
