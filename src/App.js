0afaf4d9645bdfac4171b25c73b910ccf3771131
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-28 12:28:51 -0300

    UI dependencies and initial style setting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
 
 import MainPage from 'pages/Main';
 import Login from 'pages/Login';
 
 const App = () => (
-  <BrowserRouter>
+  <React.Fragment>
+    <CssBaseline />
+    <BrowserRouter>
     <Switch>
       <Route path='/login' component={Login} />
       <Route component={MainPage} />
     </Switch>
   </BrowserRouter>
+  </React.Fragment>
+
 )
 
 export default App
