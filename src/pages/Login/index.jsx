10154d749ad761fcb5114c17a5dd440e557b337b
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-28 14:14:31 -0300

    Login screen styles (alignment)

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button, Grid } from '@material-ui/core';
 
+import { Container } from './styles'
+
 import { ReactComponent as Logo } from '../../images/logo-react-zzaria.svg'
 
 const Login = () => (
-  <React.Fragment>
-    <Grid container>
+  <Container>
+    <Grid container direction='column' alignItems='center' spacing={4}>
       <Grid item>
         <Logo />
       </Grid>
@@ -13,7 +15,7 @@ const Login = () => (
         <Button>Entrar com GitHub</Button>
       </Grid>
     </Grid>
-  </React.Fragment>
+  </Container>
 )
 
 export default Login
