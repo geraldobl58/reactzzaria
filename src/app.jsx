90261b75b28426a6edef9265fec1d0923ae11cca
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-31 10:03:35 -0300

    Rules of redirects and authentication

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,7 @@
 import React, { lazy, Suspense, useEffect, useContext } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 import { LinearProgress } from '@material-ui/core';
 
 import firebase from 'services/firebase';
@@ -9,18 +11,37 @@ import { AuthContext } from 'contexts/auth';
 const MainPage = lazy(() => import('pages/Main'))
 const Login = lazy(() => import('pages/Login'))
 
-function App() {
-  const { setUserInfo } = useContext(AuthContext);
+function App({ location }) {
+  const { userInfo, setUserInfo } = useContext(AuthContext);
+
+  const { isUserLoggedIn } = userInfo;
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
-      console.log(user)
+      console.log(user);
       setUserInfo({
         isUserLoggedIn: !!user,
         user
       });
     });
   }, [setUserInfo]);
+
+  if (isUserLoggedIn) {
+    console.log('Logado!');
+    if (location.pathname === '/login') {
+      console.log('Logado e na página de login');
+      return <Redirect to='/' />
+    } else {
+      console.log('usuário logado mas não estpa na página de login');
+    }
+  } else {
+    console.log('Não logado');
+    if (location.pathname !== '/login') {
+      return <Redirect to='/login' />
+    } else {
+      console.log('Logado e está na tela de login');
+    }
+  }
 
   return (
     <Suspense fallback={<LinearProgress />}>
@@ -30,6 +51,10 @@ function App() {
       </Switch>
     </Suspense>
   )
+}
+
+App.propTypes = {
+  location: PropTypes.object.isRequired
 }
 
 export default App;
