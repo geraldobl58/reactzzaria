90d80380cb75d611229703db461c89c3f805b847
Geraldo Luiz <geraldobl58@gmail.com>
2021-06-10 12:24:02 -0300

    Creating Checkout Page Markup

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import { Content } from './styles';
+import {  Grid, Paper, Typography } from '@material-ui/core';
+
+import { Content, PaperContainer } from './styles';
 
 function Checkout() {
   return (
     <Content>
-      <h1>Checkout</h1>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={6}>
+          <Typography variant='h6'>Qual o endereço para entrega?</Typography>
+          <Paper>
+            <PaperContainer>
+              Endereço para entrega
+            </PaperContainer>
+          </Paper>
+          <Typography variant='h6'>Qual o seu telefone?</Typography>
+          <Paper>
+            <PaperContainer>
+              Telefone
+            </PaperContainer>
+          </Paper>
+        </Grid>
+        <Grid container item xs={12} md={6} direction='column'>
+          <Typography variant='h6'>Informações do seu pedido?</Typography>
+          <Paper>
+            <PaperContainer>
+              Pedido
+            </PaperContainer>
+          </Paper>
+        </Grid>
+      </Grid>
     </Content>
   )
 }
--- src/pages/Checkout/styles.js
+++ src/pages/Checkout/styles.js
@@ -4,3 +4,11 @@ export const Content = styled.main`
   padding: 80px 20px 20px;
   flex-grow: 1;
 `
+export const Title = styled.div`
+  text-align: left;
+`
+
+export const PaperContainer = styled.div`
+  padding: ${({ theme }) => theme.spacing(2)}px;
+  flex-grow: 1;
+`
