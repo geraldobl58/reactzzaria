4bc755a07575864bc1b12934917103e2a5d4d3d2
Geraldo Luiz <geraldobl58@gmail.com>
2021-06-10 17:41:21 -0300

    Creating Address and Phone Fields

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import {  Grid, Paper, Typography } from '@material-ui/core';
+import {  Grid, Typography } from '@material-ui/core';
 
-import { Content, PaperContainer } from './styles';
+import { Content, PaperContainer, MaterialTextField } from './styles';
 
 function Checkout() {
   return (
@@ -10,29 +11,51 @@ function Checkout() {
       <Grid container spacing={4}>
         <Grid item xs={12} md={6}>
           <Typography variant='h6'>Qual o endereço para entrega?</Typography>
-          <Paper>
             <PaperContainer>
-              Endereço para entrega
+              <Grid container spacing={2}>
+                <TextField label='CEP' xs={4} autoFocus />
+                <Grid item xs={8} />
+                <TextField label='Rua' xs={9} />
+                <TextField label='Número' xs={3} />
+                <TextField label='Complemento' xs={12} />
+                <TextField label='Cidade' xs={9} />
+                <TextField label='Estado' xs={3} />
+              </Grid>
             </PaperContainer>
-          </Paper>
           <Typography variant='h6'>Qual o seu telefone?</Typography>
-          <Paper>
             <PaperContainer>
-              Telefone
+              <TextField label='Telefone' xs={4} />
             </PaperContainer>
-          </Paper>
         </Grid>
         <Grid container item xs={12} md={6} direction='column'>
           <Typography variant='h6'>Informações do seu pedido?</Typography>
-          <Paper>
             <PaperContainer>
               Pedido
             </PaperContainer>
-          </Paper>
         </Grid>
       </Grid>
     </Content>
   )
+}
+
+function TextField({ xs, autoFocus, ...props }) {
+  return (
+    <Grid item xs={xs}>
+      <MaterialTextField
+        fullWidth
+        variant='outlined'
+        inputProps={{
+          autoFocus
+        }}
+        {...props}
+      />
+    </Grid>
+  )
+}
+
+TextField.propTypes = {
+  autoFocus: PropTypes.bool,
+  xs: PropTypes.number
 }
 
 export default Checkout;
--- src/pages/Checkout/styles.js
+++ src/pages/Checkout/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { Paper, TextField } from '@material-ui/core';
+
 export const Content = styled.main`
   padding: 80px 20px 20px;
   flex-grow: 1;
@@ -8,7 +10,10 @@ export const Title = styled.div`
   text-align: left;
 `
 
-export const PaperContainer = styled.div`
+export const PaperContainer  = styled(Paper)`
   padding: ${({ theme }) => theme.spacing(2)}px;
+  margin-bottom: ${({ theme }) => theme.spacing(5)}px;
   flex-grow: 1;
 `
+
+export const MaterialTextField = styled(TextField)``
