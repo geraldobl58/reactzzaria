37cf7a8c7ec26b7f0cdc681f10c3ab80b06abab2
Geraldo Luiz <geraldobl58@gmail.com>
2021-06-17 09:12:39 -0300

    Header for order confirmation page

--- src/pages/Confirmation/index.jsx
+++ src/pages/Confirmation/index.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 import Content from 'components/Content';
 
+import { Typography } from '@material-ui/core';
+
+import { Title } from './styles';
+
+import { useAuth } from 'hooks';
+
 function CheckoutConfirmation() {
+  const { userInfo } = useAuth();
+
   return (
     <Content>
-      Checkout
+      <Title>
+        <Typography variant="h4" gutterBottom>
+          Oi {userInfo.user.firstName}!
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Confere, por favor, se está tudo certo com seu pedido antes de finalizar?
+        </Typography>
+      </Title>
     </Content>
   )
 }
