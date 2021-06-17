26827065c9625a46fe7d0a914dc73bfd2934aa3e
Geraldo Luiz <geraldobl58@gmail.com>
2021-06-17 11:39:33 -0300

    Adding address and phone number on the order confirmation screen

--- src/pages/Confirmation/index.jsx
+++ src/pages/Confirmation/index.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { useAuth } from 'hooks';
 
 import Content from 'components/Content';
+import OrderInfo from 'components/OrderInfo';
 
 import { Typography, Paper, Container } from '@material-ui/core';
 
-import { Title, PaperContainer } from './styles';
-import OrderInfo from 'components/OrderInfo';
+import { Title, PaperContainer, MaterialDivider } from './styles';
+
 
 function CheckoutConfirmation() {
   const { userInfo } = useAuth();
@@ -29,6 +30,20 @@ function CheckoutConfirmation() {
               Seu pedido:
             </Typography>
             <OrderInfo />
+            <MaterialDivider />
+            <Typography variant="h6" gutterBottom>
+              Endereço para entrega:
+            </Typography>
+            <Typography>
+              Rua john doe, 1368
+            </Typography>
+            <MaterialDivider />
+            <Typography variant="h6" gutterBottom>
+              Telefone para contato:
+            </Typography>
+            <Typography>
+              (99)95287-1758
+            </Typography>
           </PaperContainer>
         </Paper>
       </Container>
--- src/pages/Confirmation/styles.js
+++ src/pages/Confirmation/styles.js
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
 
+import { Divider } from '@material-ui/core';
+
 export const Title = styled.div`
   text-align: center;
 `
 
 export const PaperContainer = styled.div`
   padding: ${({ theme }) => theme.spacing(3)}px;
+`
+
+export const MaterialDivider = styled(Divider)`
+  margin: ${({ theme }) => theme.spacing(3, 0)};
 `
